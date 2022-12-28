694752964e08dea8dbcaa50ddb9946bbe6c264d5
André Silva <up202108724@g.uporto.pt>
2022-12-28 18:38:35 +0000

    Qualquer coisa

---
+++ GestaoAeroporto.h
@@ -0,0 +1,14 @@
+//
+// Created by andre on 28-12-2022.
+//
+
+#ifndef PROJETOAED2_GESTAOAEROPORTO_H
+#define PROJETOAED2_GESTAOAEROPORTO_H
+
+
+class GestaoAeroporto {
+
+};
+
+
+#endif //PROJETOAED2_GESTAOAEROPORTO_H
