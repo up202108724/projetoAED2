33cfb1e46d48296d480cde45304612664e7e5fb6
up202108738 <up202108738@up.pt>
2023-01-08 12:39:36 +0000

    CompanhiaAerea documented

--- CompanhiaAerea.h
+++ CompanhiaAerea.h
@@ -14,18 +14,70 @@ using namespace std;
 
 class CompanhiaAerea {
 public:
+    /**
+    * Define uma companhia aérea, definida pela sua sigla, nome, callsign e país
+    * @brief Constructor.
+    * @brief Complexidade O(1)
+    *@param sigla
+    *@param nome
+    *@param callsign
+    *@param pais
+    */
     CompanhiaAerea(string sigla, string nome, string callsign, string pais);
+    /**
+     * @brief Obtém a sigla da companhia aérea
+     * @brief Complexidade O(1)
+     * @return sigla
+     */
     string getSigla();
+    /**
+     * @brief Devolve o nome da companhia aérea
+     * @brief Complexidade O(1)
+     * @return nome
+     */
     string getNome();
+    /**
+     * @brief Adiciona um novo voo à lista de voos da companhia aérea
+     * @brief Complexidade O(1)
+     * @param air1 Código do aeroporto de partida
+     * @param air2 Código do aeroporto de destino
+     */
+
     void addFlight(const string& air1, const string& air2);
+    /**
+     * @brief obtém a lista de voos da companhia aérea
+     * @brief Complexidade O(1)
+     * @return flights
+     */
     unordered_multimap<string,string> getFlights();
+    /**
+    * @brief Define a igualdade de companhias aéreas pela comparação entre as suas siglas
+    * @brief Complexidade O(1)
+    * @param ca
+    * @return sigla_==ca.getSigla()
+    */
     bool operator==(CompanhiaAerea ca);
 
 private:
+    /**
+     * @brief Sigla da companhia aérea
+     */
     string sigla_;
+    /**
+     * @brief Nome da companhia aérea
+     */
     string nome_;
+    /**
+     * @brief Callsign da companhia aérea
+     */
     string callsign_;
+    /**
+     * @brief País da companhia aérea
+     */
     string pais_;
+    /**
+     * @brief Lista de voos da companhia aérea
+     */
     unordered_multimap<string, string> flights;
 };
 
